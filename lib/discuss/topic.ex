2c397f82f719512e6f70d839d23fa915540b317d
frunox <jrcannon@msn.com>
2022-03-03 17:41:29 -0600

    initial commit w/ Udemy class code + nimble_publisher

---
+++ lib/discuss/topic.ex
@@ -0,0 +1,20 @@
+defmodule Discuss.Topic do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "topics" do
+    field :title, :string, unique: true
+    belongs_to :user, Discuss.User
+    has_many :comments, Discuss.Comment
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(topic, attrs \\ %{}) do
+    topic
+    |> cast(attrs, [:title])
+    |> validate_required([:title])
+    |> unique_constraint(:title)
+  end
+end
---
+++ lib/discuss_web/channels/comments_channel.ex
@@ -0,0 +1,54 @@
+defmodule DiscussWeb.CommentsChannel do
+  use DiscussWeb, :channel
+
+  import Ecto
+
+  alias Discuss.{Topic, Comment}
+
+  @impl true
+  def join("comments:" <> topic_id, _payload, socket) do
+    topic_id = String.to_integer(topic_id)
+
+    topic =
+      Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(comments: [:user])
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
+  end
+
+  # V134 handle_in for adding new comment ('comment:add')
+  def handle_in("comment:add", %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+    user_id = socket.assigns.user
+
+    changeset =
+      topic
+      |> build_assoc(:comments, user_id: user_id)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
+        {:reply, :ok, socket}
+
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  @impl true
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (comments:lobby).
+  @impl true
+  def handle_in("shout", payload, socket) do
+    broadcast(socket, "shout", payload)
+    {:noreply, socket}
+  end
+end
